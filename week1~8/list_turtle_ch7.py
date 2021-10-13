9f182db87b052a514f09444174786e4e6c95eb78
yeonwoo <83701837+cyanindy@users.noreply.github.com>
2021-10-14 01:09:02 +0900

    add annotate

--- week1~8/list_turtle_ch7.py
+++ week1~8/list_turtle_ch7.py
@@ -25,7 +25,7 @@ if __name__ == "__main__" :
         PlayerTurtles.append([myTurtle, tX, tY, tSize, r, g, b, xy_sum])
 
     #PlayerTurtles.sort(key=lambda x : x[1]+x[2])
-    re_tur = sorted(PlayerTurtles, key=lambda turtles : turtles[7] ,reverse = True)
+    re_tur = sorted(PlayerTurtles, key=lambda turtles : turtles[7] ,reverse = True) #구글링 사용
     #print(PlayerTurtles)
     
     for index, tList in enumerate(PlayerTurtles[0:]) :
@@ -37,8 +37,8 @@ if __name__ == "__main__" :
         if index == 0 :
             myTurtle.goto(tList[1], tList[2])
             continue
-        myTurtle.goto(re_tur[index-1][1], re_tur[index-1][2]) # 선을 그을 거북이를 이 전의 거북이 위치로 이동
+        myTurtle.goto(re_tur[index-1][1], re_tur[index-1][2]) # 선을 그을 거북이를 이 전의 거북이 위치로 이동 - googling
         myTurtle.pendown()
-        myTurtle.goto(tList[1], tList[2])   #설정된 거북이의 좌표로 이동하면서 선 긋기
+        myTurtle.goto(tList[1], tList[2])   #설정된 거북이의 좌표로 이동하면서 선 긋기 - googling
     
     turtle.done()
